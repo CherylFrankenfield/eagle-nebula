5cc2e56e81255729aa2f33c5d4f00ad6a3df8b5c
Cheryl Frankenfield and Colin Worf <colin@>
2017-11-30 09:50:08 -0800

    Add script and CDN files and sticky header Foundation block

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Backend
+
+
+
+
+// Interface
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+      $("body").addClass("sticky-shrinknav-wrapper");
+    } else{
+      $("body").removeClass("sticky-shrinknav-wrapper");
+    }
+  });
+});
